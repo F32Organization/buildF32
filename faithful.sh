3b08c3d5f6f5f7679695e45b2d7deb9895fcaa17
Alex Wilde <shreddinmachine@gmail.com>
2014-01-19 18:17:21 -0800

    Add comments.

--- faithful.sh
+++ faithful.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-GIT_DIR="/usr/share/nginx/faithful/git"
-WWW_DIR="/usr/share/nginx/faithful/www"
-ZIP_FILE="latest.zip"
 
+rem This sets the directory that Git is in.
+GIT_DIR="/usr/share/nginx/faithful/git"
+rem This sets the directory the web server serves to the public
+WWW_DIR="/usr/share/nginx/faithful/www"
+rem This sets the name of the zip file.
+ZIP_FILE="F32.zip"
+
+rem Switch to Git directory.
 cd "$GIT_DIR"
+rem If Git is already up to date and the output zip already exists, cancel.
 if (( $(git pull | grep -c 'Already up-to-date') ))
 then
   if [ -e "$WWW_DIR/$ZIP_FILE" ]
@@ -12,9 +18,12 @@ then
   fi
 fi
 
+rem Delete old zip output.
 rm "$ZIP_FILE"
+rem Create output zip with no compression and only include the selected files.
 zip -0 -rq "$ZIP_FILE" assets pack.mcmeta pack.png README.md
 
+rem If existing zip output matches new output, cancel.
 if [ -e "$WWW_DIR/$ZIP_FILE" ]
 then
   if [ $(md5sum "$ZIP_FILE" | awk '{print $1}') == $(md5sum "$WWW_DIR/$ZIP_FILE" | awk '{print $1}') ]
@@ -23,5 +32,6 @@ then
   fi
 fi
 
+rem Copy new output zip to public location.
 cp -f "$ZIP_FILE" "$WWW_DIR/$ZIP_FILE"
 exit 0
