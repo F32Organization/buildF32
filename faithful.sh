5df5355e139348ea24cd041a4de8ea2064fcd7a0
Alex Wilde <shreddinmachine@gmail.com>
2014-11-30 20:49:35 -0500

    Minor update to use variables more.

--- faithful.sh
+++ faithful.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-#Switch to fit directory.
+DL1="164.zip"
+DL2="1710.zip"
+SUM1="new164sum.md5"
+SUM2="new1710sum.md5"
+SUM3="old164sum.md5"
+SUM4="old1710sum.md5"
+GIT="/var/www/html/faithful/git"
+
+#Switch to git directory.
 cd ./git
 #Fetch latest Git repositories.
-wget --no-verbose https://github.com/F32Organization/Faithful32-1.6.4/archive/master.zip -O 164.zip
-wget --no-verbose https://github.com/F32Organization/Faithful32-1.7.10/archive/master.zip -O 1710.zip
+wget --no-verbose https://github.com/F32Organization/Faithful32-1.6.4/archive/master.zip -O $DL1
+wget --no-verbose https://github.com/F32Organization/Faithful32-1.7.10/archive/master.zip -O $DL2
 #Generate MD5 checksums for new downloaded files.
-md5sum 164.zip > new164sum.md5
-md5sum 1710.zip > new1710sum.md5
+md5sum $DL1 > $SUM1
+md5sum $DL2 > $SUM2
 #Strip file names from new checksum files before comparing sums. (Depends on replace command installed by package "mysql-server").
-replace "164.zip" "" -- new164sum.md5
-replace "1710.zip" "" -- new1710sum.md5
+replace "164.zip" "" -- $SUM1
+replace "1710.zip" "" -- $SUM2
 #Compare MD5 checksums of new downloaded files to old downloaded files. This decides which (if any) portions of the script need to run.
 #1.6.4 Handling.
-if diff old164sum.md5 new164sum.md5 >/dev/null ; then
+if diff $SUM3 $SUM1 >/dev/null ; then
 	echo Skipping 1.6.4 build portion.
 	#Delete the new download and checksum because they are unused.
-	rm 164.zip
-	rm new164sum.md5
+	rm $DL1
+	rm $SUM1
 else
 	#Unpack downloaded .zip file.
-	unzip 164.zip
+	unzip $DL1
 	#Switch to unpacked directory for 1.6.4.
 	cd ./Faithful32-1.6.4-master
 	#Remove TODO folder and any *.md files.
@@ -30,30 +38,30 @@ else
 	#Copy .zip file to web directory, replacing old version forcefully.
 	cp -f F32-1.6.4.zip /var/www/html/faithful
 	#Switch to git directory.
-	cd /var/www/html/faithful/git
+	cd $GIT
 	#Remove unpacked folders.
 	rm -r Faithful32-1.6.4-master
 	#Delete old checksum file then replace it with the new one.
-	rm old164sum.md5
-	mv new164sum.md5 old164sum.md5
+	rm $SUM3
+	mv $SUM1 $SUM3
 	#Strip filename from checksum.
-	replace "164.zip" "" -- old164sum.md5
+	replace "$164.zip" "" -- $SUM3
 	#Delete old downloaded file.
-	rm 164.zip
+	rm $DL1
 fi
 
 #Switch back to git directory.
-cd /var/www/html/faithful/git
+cd $GIT
 
 #1.7.10 Handling.
-if diff old1710sum.md5 new1710sum.md5 >/dev/null ; then
+if diff $SUM4 $SUM2 >/dev/null ; then
 	echo Skipping 1.7.10 build portion.
 	#Delete the new download and checksum because they are unused.
-	rm 1710.zip
-	rm new1710sum.md5
+	rm $DL2
+	rm $SUM2
 else
 	#Unpack downloaded .zip file.
-	unzip 1710.zip
+	unzip $DL2
 	#Switch to unpacked directory for 1.7.10.
 	cd ./Faithful32-1.7.10-master
 	#Remove TODO folder and any *.md files.
@@ -63,16 +71,16 @@ else
 	#Copy .zip file to web directory, replacing old version forcefully.
 	cp -f F32-1.7.10.zip /var/www/html/faithful
 	#Switch to git directory.
-	cd /var/www/html/faithful/git
+	cd $GIT
 	#Remove unpacked folders.
 	rm -r Faithful32-1.7.10-master
 	#Delete old checksum file then replace it with the new one.
-	rm old1710sum.md5
-	mv new1710sum.md5 old1710sum.md5
+	rm $SUM4
+	mv $SUM2 $SUM4
 	#Strip filename from checksum.
-	replace "1710.zip" "" -- old1710sum.md5
+	replace "1710.zip" "" -- $SUM4
 	#Delete old downloaded file.
-	rm 1710.zip
+	rm $DL2
 fi
 
 #Exit the script with code 0 to indicate success. (Not currently used)
--- manual.sh
+++ manual.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if pidof -x "faithful.sh" >/dev/null; then
-	echo "Faithful.sh is currently running, please wait."
+	echo "Build script is currently running, please wait."
 else
 	bash faithful.sh
 fi
