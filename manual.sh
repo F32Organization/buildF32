44f2d4848c751b53445990cc6f51be711e8a58c4
Alex Wilde <shreddinmachine@gmail.com>
2015-02-17 20:20:42 -0500

    Completely forgot to change script name.

--- manual.sh
+++ manual.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if pidof -x "faithful.sh" >/dev/null; then
+if pidof -x "buildF32.sh" >/dev/null; then
 	echo "Build script is currently running, please wait."
 else
 	bash faithful.sh
